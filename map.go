41c92eeace171e4c95f1cf865136bc32ab6550b2
yxz6 <yxzbox@gmail.com>
2023-02-26 13:59:09 -0500

    fast commit

---
+++ map.go
@@ -0,0 +1,47 @@
+package utils
+
+func CloneMap[K comparable, V any](m map[K]V) map[K]V {
+	res := make(map[K]V, len(m))
+	for k, v := range m {
+		res[k] = v
+	}
+	return res
+}
+
+func MergeMap[K comparable, V any](m1 map[K]V, m2 map[K]V) map[K]V {
+	res := make(map[K]V, len(m1)+len(m2))
+	for k, v := range m1 {
+		res[k] = v
+	}
+
+	for k, v := range m2 {
+		res[k] = v
+	}
+	return res
+}
+
+func GetMapKeys[K comparable, V any](m map[K]V) []K {
+	a := make([]K, 0, len(m))
+	for k := range m {
+		a = append(a, k)
+	}
+	return a
+}
+
+func GetMapValues[K comparable, V any](m map[K]V) []V {
+	a := make([]V, 0, len(m))
+	for _, v := range m {
+		a = append(a, v)
+	}
+	return a
+}
+
+func GetKeysAndValues[K comparable, V any](m map[K]V) ([]K, []V) {
+	kl := make([]K, 0, len(m))
+	vl := make([]V, 0, len(m))
+	for k, v := range m {
+		kl = append(kl, k)
+		vl = append(vl, v)
+	}
+	return kl, vl
+}
