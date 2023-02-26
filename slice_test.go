c09a22761c72ba5a510837e3d3516b15f573113c
yxz6 <yxzbox@gmail.com>
2023-02-26 12:37:06 -0500

    fast commit

--- slice_test.go
+++ slice_test.go
@@ -1,34 +1,31 @@
-package utils_test
+package utils
 
 import (
 	"testing"
-
-	"code.olapie.com/sugar/v2/slices"
-	"code.olapie.com/sugar/v2/testutil"
 )
 
 func TestReverse(t *testing.T) {
 	t.Run("IntArray", func(t *testing.T) {
 		a := []int{1, 2, 3, -9, 10, 1, 101}
-		slices.Reverse(a)
-		testutil.Equal(t, []int{101, 1, 10, -9, 3, 2, 1}, a)
+		ReverseSlice(a)
+		MustEqualT(t, []int{101, 1, 10, -9, 3, 2, 1}, a)
 
 		a = []int{1}
-		slices.Reverse(a)
-		testutil.Equal(t, []int{1}, a)
+		ReverseSlice(a)
+		MustEqualT(t, []int{1}, a)
 
 		a = []int{}
-		slices.Reverse(a)
-		testutil.Equal(t, []int{}, a)
+		ReverseSlice(a)
+		MustEqualT(t, []int{}, a)
 
 		a = []int{1, 3}
-		slices.Reverse(a)
-		testutil.Equal(t, []int{3, 1}, a)
+		ReverseSlice(a)
+		MustEqualT(t, []int{3, 1}, a)
 	})
 
 	t.Run("StringArray", func(t *testing.T) {
 		a := []string{"a", "b", "c", "d"}
-		slices.Reverse(a)
-		testutil.Equal(t, []string{"d", "c", "b", "a"}, a)
+		ReverseSlice(a)
+		MustEqualT(t, []string{"d", "c", "b", "a"}, a)
 	})
 }
