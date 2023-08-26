f69dca110e53cd3503361fb2d282d718e03323e5
yxz <yxzbox@gmail.com>
2023-08-26 12:35:07 -0400

    feat: add ObjectFactoryE

--- factory.go
+++ factory.go
@@ -5,38 +5,72 @@ import (
 	"sync"
 )
 
-type NamedInstanceInitializer[T any] func(ctx context.Context, key string) (T, error)
-
-type NamedInstanceFactory[T any] struct {
-	mu          sync.RWMutex // one lock is okay, as most cases are just read lock
-	cache       map[string]T
-	initializer NamedInstanceInitializer[T]
+type ObjectInitFuncTypes[T any] interface {
+	func(ctx context.Context, id string) (T, error) | func(ctx context.Context, id string) T
 }
 
-func NewNamedInstanceFactory[T any](initializer NamedInstanceInitializer[T]) *NamedInstanceFactory[T] {
-	return &NamedInstanceFactory[T]{
+type ObjectFactory[T any] struct {
+	mu          sync.RWMutex // one lock is okay, as most cases are just read lock
+	cache       map[string]T
+	initializer func(ctx context.Context, id string) T
+}
+
+func NewObjectFactory[T any](initializer func(ctx context.Context, id string) T) *ObjectFactory[T] {
+	return &ObjectFactory[T]{
 		cache:       map[string]T{},
 		initializer: initializer,
 	}
 }
 
-func (f *NamedInstanceFactory[T]) Get(ctx context.Context, name string) (instance T, err error) {
+func (f *ObjectFactory[T]) Get(ctx context.Context, id string) T {
 	f.mu.RLock()
-	instance, ok := f.cache[name]
+	obj, ok := f.cache[id]
 	f.mu.RUnlock()
 	if ok {
-		return instance, nil
+		return obj
 	}
 
 	f.mu.Lock()
 	defer f.mu.Unlock()
-	instance, ok = f.cache[name]
-	if !ok {
-		instance, err = f.initializer(ctx, name)
-		if err != nil {
-			return instance, err
-		}
-		f.cache[name] = instance
+	obj, ok = f.cache[id]
+	if ok {
+		obj = f.initializer(ctx, id)
 	}
-	return instance, nil
+	f.cache[id] = obj
+	return obj
+}
+
+type ObjectFactoryE[T any] struct {
+	mu          sync.RWMutex // one lock is okay, as most cases are just read lock
+	cache       map[string]T
+	initializer func(ctx context.Context, id string) (T, error)
+}
+
+func NewObjectFactoryE[T any, F ObjectInitFuncTypes[T]](initializer func(ctx context.Context, id string) (T, error)) *ObjectFactoryE[T] {
+	return &ObjectFactoryE[T]{
+		cache:       map[string]T{},
+		initializer: initializer,
+	}
+}
+
+func (f *ObjectFactoryE[T]) Get(ctx context.Context, id string) (obj T, err error) {
+	f.mu.RLock()
+	obj, ok := f.cache[id]
+	f.mu.RUnlock()
+	if ok {
+		return obj, nil
+	}
+
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	obj, ok = f.cache[id]
+	if ok {
+		return obj, nil
+	}
+	obj, err = f.initializer(ctx, id)
+	if err != nil {
+		return obj, err
+	}
+	f.cache[id] = obj
+	return obj, nil
 }
