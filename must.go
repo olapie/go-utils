a9f2387640286220d895f6e904d92b3773839844
yxz <yxzbox@gmail.com>
2023-08-27 11:09:40 -0400

    feat: check nil values

--- must.go
+++ must.go
@@ -1,6 +1,8 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // MustGet eliminates nil err and panics if err isn't nil
 func MustGet[T any](v T, err error) T {
@@ -38,8 +40,56 @@ func MustFalse(b bool, msgAndArgs ...any) {
 	}
 }
 
+// MustError panics if v is not nil
+func MustError(v error, msgAndArgs ...any) {
+	if v == nil {
+		return
+	}
+
+	s := fmt.Sprintf("%v", v)
+	if len(msgAndArgs) == 0 {
+		panic(s)
+	}
+	format := s + " " + fmt.Sprint(msgAndArgs[0])
+	panic(fmt.Sprintf(format, msgAndArgs[1:]...))
+}
+
+// MustNoError panics if v is nil
+func MustNoError(v error, msgAndArgs ...any) {
+	if v == nil {
+		if len(msgAndArgs) == 0 {
+			panic("")
+		}
+		panic(fmt.Sprintf(fmt.Sprint(msgAndArgs[0]), msgAndArgs[1:]...))
+	}
+}
+
+// MustNilPtr panics if v is not nil
+func MustNilPtr[T any](v *T, msgAndArgs ...any) {
+	if v == nil {
+		return
+	}
+
+	s := fmt.Sprintf("%v", v)
+	if len(msgAndArgs) == 0 {
+		panic(s)
+	}
+	format := s + " " + fmt.Sprint(msgAndArgs[0])
+	panic(fmt.Sprintf(format, msgAndArgs[1:]...))
+}
+
+// MustNotNilPtr panics if v is nil
+func MustNotNilPtr[T any](v *T, msgAndArgs ...any) {
+	if v == nil {
+		if len(msgAndArgs) == 0 {
+			panic("")
+		}
+		panic(fmt.Sprintf(fmt.Sprint(msgAndArgs[0]), msgAndArgs[1:]...))
+	}
+}
+
 // MustNil panics if v is not nil
-func MustNil(v any, msgAndArgs ...any) {
+func MustNil[T any, V []T | *T | map[string]T | map[int]T | map[int64]T](v V, msgAndArgs ...any) {
 	if v == nil {
 		return
 	}
@@ -53,7 +103,7 @@ func MustNil(v any, msgAndArgs ...any) {
 }
 
 // MustNotNil panics if v is nil
-func MustNotNil(v any, msgAndArgs ...any) {
+func MustNotNil[T any, V []T | *T | map[string]T | map[int]T | map[int64]T](v V, msgAndArgs ...any) {
 	if v == nil {
 		if len(msgAndArgs) == 0 {
 			panic("")
